c87fec4ef538a2b3a65d2bed40fab33fcb679528
sam41306061 <schoenbergwebtools@gmail.com>
2024-08-02 07:15:13 -0600

    completed rust config file and added jesttesting suite to project

--- src/bin/projector.rs
+++ src/bin/projector.rs
@@ -1,6 +1,9 @@
+use anyhow::{Ok, Result};
 use clap::Parser;
-use projector::opts::Opts;
-fn main() {
-   let opts = Opts::parse();
-   println!("{:?}", opts);
+use projector::{config::Config, opts::Opts};
+fn main() -> Result<()> {
+    let opts: Config = Opts::parse().try_into()?;
+    println!("{:?}", opts);
+
+    return Ok(());
 }
--- target/debug/deps/projector-8c0e817e6959f544.d
+++ target/debug/deps/projector-8c0e817e6959f544.d
@@ -1,8 +1,9 @@
-/Users/sam4130/Documents/GitHub/projector-ts-rust-go/target/debug/deps/libprojector-8c0e817e6959f544.rmeta: src/lib.rs src/opts.rs
+/Users/sam4130/Documents/GitHub/projector-ts-rust-go/target/debug/deps/libprojector-8c0e817e6959f544.rmeta: src/lib.rs src/opts.rs src/config.rs
 
-/Users/sam4130/Documents/GitHub/projector-ts-rust-go/target/debug/deps/libprojector-8c0e817e6959f544.rlib: src/lib.rs src/opts.rs
+/Users/sam4130/Documents/GitHub/projector-ts-rust-go/target/debug/deps/libprojector-8c0e817e6959f544.rlib: src/lib.rs src/opts.rs src/config.rs
 
-/Users/sam4130/Documents/GitHub/projector-ts-rust-go/target/debug/deps/projector-8c0e817e6959f544.d: src/lib.rs src/opts.rs
+/Users/sam4130/Documents/GitHub/projector-ts-rust-go/target/debug/deps/projector-8c0e817e6959f544.d: src/lib.rs src/opts.rs src/config.rs
 
 src/lib.rs:
 src/opts.rs:
+src/config.rs:
