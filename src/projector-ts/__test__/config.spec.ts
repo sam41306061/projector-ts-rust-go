eafba7548b1a738bdf0ed2f75991c5518c25bf13
sam41306061 <schoenbergwebtools@gmail.com>
2024-08-02 08:09:49 -0600

    addeed test case for ts, need to fix

--- src/projector-ts/__test__/config.spec.ts
+++ src/projector-ts/__test__/config.spec.ts
@@ -5,3 +5,25 @@ test("Running my first test", function () {
   expect(config.operation).toEqual(Operation.Print);
   expect(config.args).toEqual([]);
 });
+test("print key", function () {
+  const config = getConfigPath({
+    args: ["foo"],
+  });
+  expect(config.operation).toEqual(Operation.Print);
+  expect(config.args).toEqual(["foo"]);
+});
+test("add key", function () {
+  const config = getConfigPath({
+    args: ["add", "foo", "bar"],
+  });
+  expect(config.operation).toEqual(Operation.Add);
+  expect(config.args).toEqual(["foo", "bar"]);
+});
+
+test("remove key", function () {
+  const config = getConfigPath({
+    args: ["remove", "foo"],
+  });
+  expect(config.operation).toEqual(Operation.Remove);
+  expect(config.args).toEqual(["foo", "bar"]);
+});
