7d7428a4f31fc95d43f584c640a54619cecf4565
sam41306061 <schoenbergwebtools@gmail.com>
2024-08-02 07:32:51 -0600

    re-commited rust config file

--- src/config.rs
+++ src/config.rs
@@ -1,64 +1,97 @@
-use anyhow::Ok;
-use anyhow::Result;
+use std::path::PathBuf;
 
-// set enum 
+use anyhow::{anyhow, Context, Ok, Result};
+
+use crate::opts::Opts;
+#[derive(Debug)]
+pub struct Config {
+    pub operation: Operation,
+    pub config: PathBuf,
+    pub pwd: PathBuf,
+}
+
+impl TryFrom<Opts> for Config {
+    type Error = anyhow::Error;
+    fn try_from(value: Opts) -> Result<Self> {
+        let operation = value.args.try_into()?;
+        let config = get_config(value.config)?;
+        let pwd = get_config(value.pwd)?;
+
+        return Ok(Config {
+            operation,
+            config,
+            pwd,
+        });
+    }
+}
+#[derive(Debug)]
 pub enum Operation {
     Print(Option<String>),
-    Add(String,String), // tuple example ((String, String))
-    Remove(String)
+    Add(String, String), // tuple example ((String, String))
+    Remove(String),
 }
 
 impl TryFrom<Vec<String>> for Operation {
     type Error = anyhow::Error;
     fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
-        if value.len() == {
-            return  Ok(Operation::Print(None));
-        } 
-        
-        let term = value.get(0).expect(msg)
+        if value.len() == 0 {
+            return Ok(Operation::Print(None));
+        }
+
+        let mut value = value;
+
+        let term = value.get(0).expect("expect to exist!");
+        if term == "add" {
+            if value.len() != 3 {
+                let err = anyhow!("Expects 2 arguments but got:{}", value.len() - 1);
+                return Err(err);
+            }
+            let mut drain = value.drain(1..=2);
+            return Ok(Operation::Add(
+                drain.next().expect("To Exist 1"),
+                drain.next().expect("To Exist 2"),
+            ));
+        }
+        if term == "remove" {
+            if value.len() != 2 {
+                let err = anyhow!(
+                    "operation remove expects 1 arguments but got:{}",
+                    value.len() - 1
+                );
+                return Err(err);
+            }
+            let arg = value.pop().expect("To exist remove");
+            return Ok(Operation::Remove(arg));
+        }
+        if value.len() > 1 {
+            let err = anyhow!(
+                "operation print expects 0 or 1 arguments but got:{}",
+                value.len()
+            );
+            return Err(err);
+        }
+        let arg = value.pop().expect("To exist remove");
+        return Ok(Operation::Print(Some(arg)));
     }
 }
-// config structs 
-struct Config {
-    Args: Vec<String>,
-    Operation: Vec<PathBuf>,
-    Confg: Vec<String>,
-    Pwd: Vec<String>
+fn get_config(config: Option<PathBuf>) -> Result<PathBuf> {
+    // get the pathbuf , return the pathbuf
+    if let Some(v) = config {
+        return Ok(v);
+    }
+    let loc = std::env::var("XDG_CONFIG_HOME").context("unable to get xdg_config_home object")?;
+    let mut loc = PathBuf::from(loc);
+
+    loc.push("projector");
+    loc.push("projector.json");
+
+    return Ok(loc);
 }
 
+fn get_pwd(pwd: Option<PathBuf>) -> Result<PathBuf> {
+    if let Some(p) = pwd {
+        return Ok(p);
+    }
 
-// relevant functions 
-fn get_pwd(option_value: Option<PathBuf>) {
-    if let Print(value) = option_value {
-        println!("Pwd: {}", value);
-    } else {
-        println("No value provided for pwd");
-    }
-}
-fn get_config_path(option_value: Option<PathBuf>) {
-    if let Print(value) = option_value {
-        println!("Config: {}", value);
-    } else {
-        println("No value provided for config")
-    }
-}
-fn get_operations(operation: Operation) {
-    if length(operation.args) = 0 {
-        return Print;
-    }
-    if length(operation.args) = [0] {
-       return Add;
-    }
-    if length(operation.args) = [0] {
-        return Remove;
-     }
-}
-fn get_arguments(operation: Operation<i32>) {
-    if length(operation.args) = 0 {
-        return str::parse::<i32>[].expect("This is the intal setting")
-    }
-}
-
-fn new_config() {
-    // fill in later 
+    return std::env::current_dir().context("unable to get std::env::current_dir");
 }
